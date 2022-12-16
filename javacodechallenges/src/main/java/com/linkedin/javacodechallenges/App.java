0e34091d1455f2bd16c817ae1564e62053faac56
Margarita <62470332+convertIntoCode@users.noreply.github.com>
2022-12-16 13:56:33 +0000

    Implement isPasswordComplex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,16 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        String regex = ("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,32}");
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+        return m.find();
     }
 
     public static void main(String[] args) {
